ca2af9baa2c0056702d6384c738318d583e12895
collinc777 <collinc777@aol.com>
2017-03-09 16:16:40 +0000

    The run harness needs to be named application.py, because that is what Elastic Beanstalk uses to deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 # __init__.py essentially makes the folder "app" a module we can import
-# you can see a usage of this in run.py: "from app import application"
+# you can see a usage of this in application.py: "from app import application"
 # which we define down below
 
 from flask import Flask
--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@
 from flask import render_template, flash, redirect
 from app import application
 
+
 @application.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('hello.html')
