f1ef62238b17c7a86f96d19a2c418f56097f9b99
collinc777 <collinc777@aol.com>
2017-03-08 18:16:00 -0600

    Deploy

--- application.py
+++ application.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/hello')
+@application.route('/hello')
 def hello():
     return render_template('hello.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
