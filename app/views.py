ae20319dddf7bdcdcd48159e90726ff269881c45
ibly31UT <willc@Williams-MacBook-Pro.local>
2017-03-22 07:10:23 -0500

    Trimmed book data, theres now a book listing and individual book page
    Started preliminary work on getting interconnectivity working with
    links to other models. Ran into some issues with books pointing to
    character IDs that got trimmed out with trimdata.py, but I just added a
    check if that character exists before displaying it.

--- app/views.py
+++ app/views.py
@@ -5,10 +5,6 @@ from app import application
 import json
 
 navigation = [{"url": "/", "name": "Home"}, {"url": "/characters", "name": "Characters"}, {"url": "/houses", "name": "Houses"}, {"url": "/regions", "name": "Regions"}, {"url": "/books", "name": "Books"}, {"url":"/devnotes","name":"Dev Notes"}]
-names = ["Catelyn Tully", "Tyrion Lannister", "Eddard Stark"]
-houses = ["House Tully", "House Lannister", "House Stark"]
-ages = [16, 45, 60]
-status = [True, True, False]
 
 HL_CHARACTERS = 1
 HL_HOUSES = 2
@@ -24,11 +20,22 @@ def loadListing(filename):
 # properties are simply 2-element arrays of a human readable name and a dictionary key
 # e.g. ["Image", "imageLink"] -> Image (for table header) and house["imageLink"] when accessed
 
+# character_links, house_links and book_links are used to provide human readable
+# they ARE duplicated data... but they make our lives much easier
+# links between resources. E.g. in the books/2 page we want to have links to the 
+# POV (point-of-view) characters that are in it. So we pass it the character_links array
+
 character_listing = {"title": "Characters", "url": "/characters", "properties": [["Name", "name"], ["Gender", "male"], ["Culture", "culture"], ["House", "house"]]}
 character_listing["data"] = loadListing("data/trimmed_characters.json")
+character_links = {character["id"]: {"name": character["name"], "link": "/characters/" + str(character["id"])} for character in character_listing["data"]}
 
 house_listing = {"title": "Houses", "url": "/houses", "properties": [["Name", 'name'], ["Image",'imageLink'], ["Current Lord", 'currentLord'], ["Region", 'region'], ["Coat of Arms", 'coatOfArms'], ["Founded", 'founded'], ["Overlord", 'overlord'], ["Extinct?", 'isExtinct'], ["Words", 'words']]}
 house_listing["data"] = loadListing("data/api_got_show/trimmed_houses.json")
+house_links = {house["id"]: {"name": house["name"], "link": "/houses/" + str(house["id"])} for house in house_listing["data"]}
+
+book_listing = {"title": "Books", "url": "/books", "properties": [["Name", "name"], ["Publisher", "publisher"], ["Country", "country"], ["Release Date", "released"], ["Media Type", "mediaType"]]}
+book_listing["data"] = loadListing("data/api_ice_and_fire/trimmed_books.json")
+book_links = {book["id"]: {"name": book["name"], "link": "/books/" + str(book["id"])} for book in book_listing["data"]}
 
 # Build a base "context" dictionary for passing to any given template
 def createContext(nav_highlight=-1, **kwargs):
@@ -45,7 +52,7 @@ def character(charid):
         charid = int(charid)
     except:
         # Could not even convert to an integer, return empty page for now
-        return render_template('notfound.html', entity="Character", **createContext(1, entity="Character", entity_id=charid))
+        return render_template('notfound.html', **createContext(1, entity="Character", entity_id=charid))
     
     character = None
     for c in character_listing["data"]:
@@ -55,7 +62,7 @@ def character(charid):
         context = createContext(HL_CHARACTERS, entity="Character", entity_id=charid)
         return render_template('notfound.html', **context)
     else:
-        context = createContext(HL_CHARACTERS, character=character)
+        context = createContext(HL_CHARACTERS, character=character, book_links=book_links)
         return render_template('character.html', **context)
 
 @application.route("/houses/<houseid>")
@@ -64,7 +71,7 @@ def house(houseid):
         houseid = int(houseid)
     except:
         # Could not even convert to an integer, return empty page for now
-        return render_template('notfound.html', entity="House", **createContext(1, entity="House", entity_id=houseid))
+        return render_template('notfound.html', **createContext(1, entity="House", entity_id=houseid))
     
     house = None
     for h in house_listing["data"]:
@@ -76,6 +83,25 @@ def house(houseid):
     else:
         context = createContext(HL_HOUSES, house=house)
         return render_template('house.html', **context)
+
+@application.route("/books/<bookid>")
+def book(bookid):
+    try:
+        bookid = int(bookid)
+    except:
+        # Could not even convert to an integer, return empty page for now
+        return render_template('notfound.html', **createContext(1, entity="Book", entity_id=bookid))
+    
+    book = None
+    for b in book_listing["data"]:
+        if b["id"] == bookid:
+            book = b
+    if book is None:
+        context = createContext(HL_BOOKS, entity="Book", entity_id=bookid)
+        return render_template('notfound.html', **context)
+    else:
+        context = createContext(HL_HOUSES, book=book, character_links=character_links)
+        return render_template('book.html', **context)
 
 @application.route('/characters', methods=['GET', 'POST'])
 def characters():
@@ -94,7 +120,7 @@ def regions():
 
 @application.route('/books', methods=['GET', 'POST'])
 def books():
-    context = createContext(HL_BOOKS, listing=character_listing)
+    context = createContext(HL_BOOKS, listing=book_listing)
     return render_template('listing.html', **context)
 
 @application.route('/devnotes', methods=['GET', 'POST'])
