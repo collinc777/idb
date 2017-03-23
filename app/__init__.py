1c58f1ac4cdd1f1538dbb240b2f82248bbf79b8d
Greg McDonald <grm695@utexas.edu>
2017-03-22 21:46:05 -0500

    Added Flask-SQLAlchemy.

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,12 @@
 # which we define down below
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
+# Update this to the URI of our database in IDB2
+# application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+database = SQLAlchemy(application)
 
 from app import views
