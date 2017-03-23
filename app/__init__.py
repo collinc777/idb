b2b1e2cfd8cc113851628dcdff643bc1b16485c4
collinc777 <collinc777@aol.com>
2017-03-23 15:02:55 -0500

    fix sqlalchemy and change name of tests file to spec

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
 # __init__.py essentially makes the folder "app" a module we can import
 # you can see a usage of this in application.py: "from app import application"
 # which we define down below
-
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
 # Update this to the URI of our database in IDB2
 # application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-#database = SQLAlchemy(application)
-
+database = SQLAlchemy(application)
 from app import views
