3d31cc0e60f939181a54c6fc8febcd804153c341
Greg McDonald <grm695@utexas.edu>
2017-03-23 18:09:31 -0500

    Added SQLAlchemy tests, and Alliance model tests. Can't be run yet.

--- app/tests.py
+++ app/tests.py
@@ -12,7 +12,9 @@
 # -------
 
 from unittest import main, TestCase
+from app import database
 from app import models
+from app import views
 
 # -----------
 # TestModels
@@ -22,14 +24,14 @@ from app import models
 class TestModels (TestCase):
 
     def setUp(self):
-        # Perform database setup, nothing to do for IDB1
+        # Perform database setup, nothing to do for Idatabase1
         pass
 
         # Taken from: http://kronosapiens.github.io/blog/2014/07/29/setting-up-unit-tests-with-flask.html
         # app.config.from_object('webapp.config.Testing')
-        # db.session.close()
-        # 3db.drop_all()
-        # db.create_all()
+        # database.session.close()
+        # 3database.drop_all()
+        # database.create_all()
 
     # -----
     # Book
@@ -59,6 +61,21 @@ class TestModels (TestCase):
             "United States", [148, 208, 232], "George R.R. Martin", "Hardcover", "1996", [2, 12, 13])
         self.assertEqual(b.publisher, "Bantam Books")
 
+    def test_Book_add(self):
+        exampleBook = models.Book(
+            694, "978-0553103540", "A Game of Thrones", "Bantam Books",
+                        "United States", [148, 208, 232], "George R.R. Martin", "Hardcover", "1996", [2, 12, 13])
+
+        database.session.add(exampleBook)
+        database.session.commit()
+
+        book = database.session.query(models.Book).filter_by(name="A Game of Thrones").first()
+        self.assertEqual(book.numberOfPages, 694)
+        self.assertEqual(book.isbn, "978-0553103540")
+
+        database.session.delete(exampleBook)
+        database.session.commit()
+
     # ----------
     # Character
     # ----------
@@ -82,6 +99,19 @@ class TestModels (TestCase):
         c = models.Character("Night's Watch", "", [], "", "", ["Sam"], False,
                              "Samwell Tarly", "", "Male", "", [], [], [], [], [], [], "", True)
         self.assertEqual(c.titles, [])
+
+    def test_Character_add(self):
+        exampleCharacter = models.Character("Night's Watch", "", [], "", "", ["Sam"], False,
+                             "Samwell Tarly", "", "Male", "", [], [], [], [], [], [], "", True)
+
+        database.session.add(exampleCharacter)
+        database.session.commit()
+
+        character = database.session.query(models.Character).filter_by(name="Samwell Tarly").first()
+        self.assertEqual(character.house, "Night's Watch")
+
+        database.session.delete(exampleCharacter)
+        database.session.commit()
 
     # -----
     # House
@@ -114,6 +144,38 @@ class TestModels (TestCase):
                          [955, 1270, 1272, 1350, 1356, 1442, 1568, 1814, 1852, 1994, 2012], "The Westerlands", [])
         self.assertEqual(
             h.swornMember_ids, [955, 1270, 1272, 1350, 1356, 1442, 1568, 1814, 1852, 1994, 2012])
+
+    def test_House_add(self):
+        # exampleHouse = models.House(1, 2, 3, ["cadetBranches"], "founded", "diedOut", ["title"], "coatOfArms", "words", ["seats"], 4, "name", [5, 6], "region", ["ancestralWeapons"])
+        exampleHouse = models.House("", 1272, "", [], "", "", ["Ser"],
+                         "Three dogs on a yellow field(Or, three dogs courant in pale sable)",
+                         "", ["Clegane's Keep"], 229, "House Clegane",
+                         [955, 1270, 1272, 1350, 1356, 1442, 1568, 1814, 1852, 1994, 2012], "The Westerlands", [])
+
+        database.session.add(exampleHouse)
+        database.session.commit()
+
+        house = database.session.query(models.House).filter_by(name="House Clegane").first()
+        self.assertEqual(house.region, "The Westerlands")
+
+        database.session.delete(exampleHouse)
+        database.session.commit()
+
+    # ---------
+    # Alliance
+    # ---------
+
+    def test_Alliance_add(self):
+        exampleAlliance = models.Alliance(1, [2, 3], ["weapons"], ["seats"], ["regions"], ["cultures"])
+        
+        database.session.add(exampleAlliance)
+        database.session.commit()
+
+        house = database.session.query(models.Alliance).filter_by(headLeader_id=1).first()
+        self.assertEqual(house.weapons, ["weapons"])
+
+        database.session.delete(exampleAlliance)
+        database.session.commit()
 
 # ----
 # main
