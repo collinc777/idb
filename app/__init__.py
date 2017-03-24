7c9613955e181fb0feefeaea60e32b4bac56a39f
collinc777 <collinc777@aol.com>
2017-03-23 21:16:25 -0500

    add alchemy stuff

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,11 @@
 # you can see a usage of this in application.py: "from app import application"
 # which we define down below
 from flask import Flask
-#from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
 # Update this to the URI of our database in IDB2
-# application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-#database = SQLAlchemy(application)
+database = SQLAlchemy(application)
 from app import views
