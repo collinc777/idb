71e2ba48bb04d5b33f1a2045c75c91d020509f91
Greg McDonald <grm695@utexas.edu>
2017-04-04 15:55:06 -0500

    Expanded database printing to print all tables and model instances.

--- app/scripts/print_database.py
+++ app/scripts/print_database.py
@@ -3,19 +3,49 @@
 from app import database
 from app.models import Book
 
-# Print all tables.
+# Get all class, model, and table names.
+classes = []
+models = []
+tables = []
+for c in database.Model._decl_class_registry.values():
+    try:
+        tables.append(c.__tablename__)
+        classes.append(c)
+    except:
+        pass
+for table in database.metadata.tables.items():
+    if table[0] in tables:
+        models.append(classes[tables.index(table[0])])
+
+# Print all database characteristics.
+# String to place at start of a title line.
 fronter = ' ----- '
+# String to place at end of a title line.
 ender = fronter
+
+# Print all classes.
+print(fronter + 'Class(es)' + ender)
+for c in classes:
+	print(c)
+print('')
+# Print all models.
+print(fronter + 'Model(s)' + ender)
+for model in models:
+	print(model)
+print('')
+# Print all tables.
 print(fronter + 'Table(s)' + ender)
-tableKeys = database.metadata.tables.keys()
-for tableKey in tableKeys:
-	print(tableKey)
+for tableName in tables:
+	print(tableName)
 print('')
 
-# Print all Book model instances.
-allBooks = Book.query.all()
-for book in allBooks:
-    print(book.toJSON())
+# Print all model instances.
+for model in models:
+	print(fronter + str(model) + ' Instances' + ender)
+	modelInstances = model.query.all()
+	for instance in modelInstances:
+		print(instance.toJSON())
+	print('')
 
 """
 source: http://stackoverflow.com/questions/6473925/sql-alchemy-getting-a-list-of-tables
