e8ed458737809b2ca60b261b9590a699292c4c66
ibly31UT <willc@Williams-MacBook-Pro.local>
2017-04-04 21:04:42 -0500

    Messing around with using query parameters to actually call database queries
    Will parameterize it after more data gets added to DB, and this breaks
    every page besides books for now.

--- app/models.py
+++ app/models.py
@@ -104,6 +104,19 @@ class Book(database.Model):
         self.released = released
         self.character_ids = character_ids
 
+    @staticmethod
+    def getSorts():
+        return ["Name", "Author", "Publisher", "ISBN", "Number of Pages", "Release Date"]
+
+    @staticmethod
+    def convertSort(sort):
+        names = ["name", "author", "publisher", "isbn", "numberOfPages", "released"]
+        for i, s in enumerate(Book.getSorts()):
+            if s == sort:
+                return names[i]
+        return None
+
+
     def toJSON(self):
         jsonString = '{'
         jsonString += '\"numberOfPages\":' + str(self.numberOfPages) + ','
@@ -131,7 +144,7 @@ class Book(database.Model):
             jsonString = jsonString[0:-1] + ']'
         else:
             jsonString += '\"characters\":[]'
-
+        
         jsonString += '}'
         return jsonString
 
