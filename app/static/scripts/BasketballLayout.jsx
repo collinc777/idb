19eeb70ba5750895ec669ace1293bf1e9e9cd0db
collinc777 <collinc777@aol.com>
2017-04-18 17:25:20 -0500

    add that last part

--- app/static/scripts/BasketballLayout.jsx
+++ app/static/scripts/BasketballLayout.jsx
@@ -249,15 +249,97 @@ class TeamLayout extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: props.teamData
+            data: props.teamData.teams
         }
     }
 
     render() {
         return (
-            <div></div>
+            <div id="teamChart"></div>
         )
     }
+
+    componentDidMount() {
+        let teams = this.state.data;
+
+        let margin = {top: 20, right: 30, bottom: 100, left: 40},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        let yScale = d3.scaleLinear()
+            .range([height, 0])
+            .domain([0, d3.max(teams, function (d) {
+                return d.PPG;
+            })]);
+        let xScale = d3.scaleBand()
+            .rangeRound([0, width], .1)
+            .domain(teams.map(function (d) {
+                return d.Name;
+            }));
+        let xAxis = d3.axisBottom(xScale);
+
+        let yAxis = d3.axisLeft(yScale);
+
+        let myChart = d3.select('#playerChart')
+            .append('svg')
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        myChart.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-91)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .style("fill", "white")
+            .style("font-size", 15)
+            .text("PPG");
+
+        myChart.selectAll(".bar")
+            .data(teams)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function (d) {
+                return xScale(d.Name);
+            })
+            .attr("y", function (d) {
+                return yScale(d.PPG);
+            })
+            .attr("height", function (d) {
+                return height - yScale(d.PPG);
+            })
+            .attr("width", xScale.bandwidth());
+
+        myChart.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .attr("y", 0)
+            .attr("x", 9)
+            .attr("dy", ".35em")
+            .attr("transform", "rotate(90)")
+            .style("text-anchor", "start")
+            .style("fill", "white");
+        myChart.append("text")
+            .attr("x", (width / 2))
+            .attr("y", 10 - (margin.top / 2))
+            .attr("text-anchor", "middle")
+            .style("font-size", "16px")
+            .style("text-decoration", "underline")
+            .style("fill", "white")
+            .text("Avg Points scored per team");
+
+        function type(d) {
+            d.wins = +d.wins; // coerce to number
+            return d;
+        }
+    }
+
 
 }
 
