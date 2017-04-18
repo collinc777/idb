62f3dc2e728516854f6cd71bd387da802df2aa80
collinc777 <collinc777@aol.com>
2017-04-17 23:18:02 -0500

    i now have a useless generic chart that don't do shit with real data

--- app/static/scripts/BasketballLayout.jsx
+++ app/static/scripts/BasketballLayout.jsx
@@ -29,15 +29,148 @@ class GameLayout extends React.Component {
 class Chart extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: props.chartData,
+            chartID: props.chartID,
+            xAxis: props.xAxis,
+            yAxis: props.yAxis
+        }
     }
 
     render() {
         return (
-            <div>
+            <div id={this.state.chartID}>
 
             </div>
         )
     }
+
+    componentDidMount() {
+        console.log("the egg");
+        var actualData = this.state.data;
+        var myData = [100, 125, 320, 440, 500, 250, 320, 720, 50];
+
+        var margin = {
+            top: 30,
+            right: 30,
+            bottom: 40,
+            left: 50
+        }
+        var height = 500 - margin.top - margin.bottom;
+        var width = 500 - margin.right - margin.left;
+        var animateDuration = 700;
+        var animateDelay = 30;
+        // var barWidth = 35;
+        // var barOffset = 5;
+        var tooltip = d3.select('body').append('div')
+            .style('position', 'absolute')
+            .style('background', '#f4f4f4')
+            .style('padding', '5 15px')
+            .style('border', '1px #333 solid')
+            .style('border-radius', '5px')
+            .style('opacity', '0');
+
+        var yScale = d3.scaleLinear()
+            .domain([0, d3.max(myData)])
+            .range([0, height]);
+
+        var xScale = d3.scaleBand()
+            .domain(d3.range(0, myData.length))
+            .range([0, width]);
+
+        var colors = d3.scaleLinear()
+            .domain([0, myData.length])
+            .range(['#90ee90', '#30c230']);
+
+        var myChart = d3.select('#' + this.state.chartID)
+            .append('svg')
+            .attr('width', width + margin.right + margin.left)
+            .attr('height', height + margin.top + margin.bottom)
+            .append('g')
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+            .style('background', '#f4f4f4')
+            .selectAll('rect')
+            .data(myData)
+            .enter().append('rect')
+            .style('fill', function (d, i) {
+                return colors(i);
+            })
+            .attr('width', xScale.bandwidth())
+            .attr('height', 0)
+            .attr('x', function (d, i) {
+                return xScale(i);
+
+            })
+            .attr('y', height)
+            .on('mouseover',
+                function (d) {
+                    tooltip.transition()
+                        .style('opacity', 1)
+
+                    tooltip.html(d)
+                        .style('left', (d3.event.pageX) + 'px')
+                        .style('top', (d3.event.pageY + 'px'))
+                    d3.select(this).style('opacity', .5);
+                })
+            .on('mouseout', function (d) {
+                tooltip.transition()
+                    .style('opacity', 0)
+                d3.select(this).style('opacity', 1);
+            })
+
+        myChart.transition()
+            .attr('height', function (d) {
+                return yScale(d);
+            })
+            .attr('y', function (d) {
+                return height - yScale(d);
+            })
+            .duration(animateDuration)
+            .delay(function (d, i) {
+                return i * animateDelay
+            })
+            .ease(d3.easeElastic);
+
+        var vScale = d3.scaleLinear()
+            .domain([0, d3.max(myData)])
+            .range([height, 0]);
+
+        var hScale = d3.scaleBand()
+            .domain(d3.range(0, myData.length))
+            .range([0, width]);
+
+        var vAxis = d3.axisLeft(vScale)
+            .ticks(5)
+            .tickPadding(5)
+
+        var vGuide = d3.select('svg')
+            .append('g')
+        vAxis(vGuide)
+        vGuide.attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+        vGuide.selectAll('path')
+            .style('fill', 'none')
+            .style('stroke', '#000')
+        vGuide.selectAll('line')
+            .style('stroke', '#000')
+
+        var hAxis = d3.axisBottom(hScale)
+            .tickValues(hScale.domain().filter(function (d, i) {
+                return !(i % (myData.length / 5));
+            }));
+
+        var hGuide = d3.select('svg')
+            .append('g')
+        hAxis(hGuide)
+        hGuide.attr('transform', 'translate(' + margin.left + ',' + (height + margin.top) + ')')
+        hGuide.selectAll('path')
+            .style('fill', 'none')
+            .style('stroke', '#000')
+        hGuide.selectAll('line')
+            .style('stroke', '#000')
+
+    }
+
+
 }
 
 class PlayerLayout extends React.Component {
@@ -48,40 +181,10 @@ class PlayerLayout extends React.Component {
         }
     }
 
-    componentDidMount() {
-        console.log("the egg");
-
-        var myData = [100, 125, 320, 440, 500];
-        var height = 500;
-        var width = 500;
-        var barWidth = 35;
-        var barOffset = 5;
-
-        var myChart = d3.select('#chart').append('svg').attr('width', width)
-            .attr('height', height)
-            .style('background', '#f4f4f4')
-            .selectAll('rect')
-            .data(myData)
-            .enter().append('rect')
-            .style('fill', 'green')
-            .attr('width', barWidth)
-            .attr('height', function (d) {
-                return d;
-            })
-            .attr('x', function (d, i) {
-                return i * (barWidth + barOffset);
-
-            })
-            .attr('y', function (d) {
-                return height - d;
-            })
-    }
-
     render() {
         console.log("the chicken");
         return (
-            <div id="chart">
-            </div>
+            <Chart chartID="playerChart" chartData={this.state.data}/>
         )
     }
 }
