be947b804bdf36e9316a435185fb2b683ac719c6
Brian Zhu <brianzhu@wireless-10-147-50-33.public.utexas.edu>
2017-04-20 16:40:08 -0500

    fixed CORS issue

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@
 # which we define down below
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 
 application = Flask(__name__)
+CORS(application)
 application.config.from_object('config')
 database = SQLAlchemy(application)
 
